d7c7ca90668f6f94baf47590b4618bfd370c2ba7
lephudat <lephucdat2004@gmail.com>
2025-11-10 15:46:48 +0700

    Add APK file

--- config/apiConfig.ts
+++ config/apiConfig.ts
@@ -1,5 +1,5 @@
 // Auto-generated API configuration
-// Generated on: 11/7/2025, 2:14:27 PM
+// Generated on: 11/10/2025, 3:44:56 PM
 // Detected IP: 192.168.100.194
 
 export const API_BASE_URL = 'https://hornish-anisha-unsoulish.ngrok-free.dev';
